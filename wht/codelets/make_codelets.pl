580e10d84162337060bccccf437b1615ef8711ff
Michael Andrews <mjands@gmail.com>
2007-08-23 19:50:43 +0000

    Added alignment to whtgen. Simplified make_codelets.pl.
    git-svn-id: svn+ssh://tux.cs.drexel.edu/site/spiral/repos/spiral_wht_2/trunk@100 d1413626-1c2f-0410-9bee-9533c53eef12

--- wht/codelets/make_codelets.pl
+++ wht/codelets/make_codelets.pl
@@ -1,74 +1,93 @@
 #!/usr/bin/env perl
-#  default.pl
-# 
-#  short description
+#  make_codelets.pl
+#
+#  Generate codelets and register them in a C array and with the automake
+#  system.
 #
 #  Author: Michael Andrews <mjand@drexel.edu>
 #  Version: 
 #  Date:
 #
 
+# TODO 
+# These parameters should be command line arguments
+# set by ./configure --enable-maintainer-mode
 $small    = 8;
 $vector   = 2;
 $whtgen   = "../../whtgen/whtgen";
 $registry = "codelet_registry.h";
 
-$codelets = "";
-$externs  = "";
-$files    = "";
-$size     = 0;
+@codelets = ();
+$codelets = 0;
 
+# Greps through a codelet file for the function call
+sub function_name {
+  $file = shift @_;
+  $name = "";
+  open(FD, $file);
+  while (<FD>) {
+    next unless $_ =~ /\s*void\s*([\w\d]+)\s*\(.*?\)$/;
+    $name = $1;
+    last;
+  }
+  close(FD);
+  return $name;
+}
+
+# Generate unaligned and aligned codelets
 for ($i=1;$i<=$small;++$i) {
-   $files .= "s_$i.c ";
-  `$whtgen -n $i > s_$i.c`;
-   $codelets .= 
-<<TEXT
-  { "apply_small$i",
-    &apply_small$i
-  },
-TEXT
-;
-  $size++;
-;
-
-  $externs .= "extern codelet apply_small$i\;\n";
+  push(@codelets,("s_$i.c", "-n $i"));
+  $codelets++;
+  push(@codelets,("s_$i\_a.c", "-n $i -a"));
+  $codelets++;
 }
 
+# Generate aligned vectorized codelets
 $v = (log($vector) / log(2)) + 1;
-
 for ($i=$v;$i<=$small;++$i) {
-   $files .= "s_$i\_v$vector.c ";
-  `$whtgen -n $i -v $vector > s_$i\_v$vector.c`;
-   $codelets .= 
-<<TEXT
-  { "apply_small$i\_v$vector",
-    &apply_small$i\_v$vector
-  },
-TEXT
-;
-  $externs .= "extern codelet apply_small$i\_v$vector\;\n";
-  $size++;
+  push(@codelets,("s_$i\_v\_$vector\_a.c", "-n $i -v $vector -a"));
+  $codelets++;
 }
 
+$externs  = "";
+$structs  = "";
+$depends  = "";
+
+while (@codelets) {
+  $file = shift @codelets;
+  $args = shift @codelets;
+
+  print "Generate:  $whtgen $args > $args\n";
+  `$whtgen $args > $file`; # Generate codelet
+  $name = function_name($file); # Grep for codelet function name
+
+  print "Register: $name in $registry\n";
+  $depends .= "$file "; # Add to dependancies
+  $externs .= "extern codelet $name\;\n"; # Add to external declarations
+  $structs .= "  { \"$name\",\n  &$name\n  },\n"; # Add to registry struct
+}
+
+# Output the C registry array (see codelets.c)
 open(FD, ">$registry");
 print FD <<TEXT
 #include "wht.h"
 
 $externs
 
-static const int wht_codelet_registry_size = $size;
+static const int wht_codelet_registry_size = $codelets;
 
 static const codelet_entry
 wht_codelet_registry[] =
 {
-$codelets
+$structs
 };
 TEXT
 ;
 close(FD);
 
+# Output the automake dependancies (see Makefile.am)
 open(FD, ">.codelets");
-print FD "CODELETS = $files\n";
+print FD "CODELETS = $depends\n";
 close(FD);
 
 # vim: set ts=4 tw=79:
