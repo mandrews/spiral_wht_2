28a800612e8c3233d8323c5018c8ed50fe4b7ce0
Michael Andrews <mjands@gmail.com>
2007-08-27 15:55:12 +0000

    Fixed problem with vectorizing interleaved code.
    git-svn-id: svn+ssh://tux.cs.drexel.edu/site/spiral/repos/spiral_wht_2/trunk@111 d1413626-1c2f-0410-9bee-9533c53eef12

--- wht/interleave.c
+++ wht/interleave.c
@@ -2,16 +2,16 @@
 #include "codelets.h"
 
 Wht *
-wht_init_interleave(size_t n, size_t i, size_t v)
+wht_init_interleave(size_t n, size_t i)
 {
   Wht *W;
   const size_t bufsize = 20; /*apply_small%d_il%d\0*/
   char buf[bufsize]; 
 
-  if (v > 1)
-    snprintf(buf,bufsize,"apply_small%zd_v%zd_il%zd",n,v,i);
-  else
-    snprintf(buf,bufsize,"apply_small%zd_v%zd_il%zd",n,2,i);
+  snprintf(buf,bufsize,"apply_small%zd_v%zd_il%zd",n,2,i);
+#if 0
+  snprintf(buf,bufsize,"apply_small%zd_il%zd",n,i);
+#endif
 
   W            = wht_init_codelet(n);
   W->apply     = wht_get_codelet(buf);
@@ -26,21 +26,14 @@ wht_init_interleave(size_t n, size_t i, size_t v)
 Wht *
 wht_parse_interleave()
 {
-  int n,i,v;
+  int n,i;
 
-  v = 1;
   if (wht_is_codelet("smallil")) {
     i = wht_read_int();
-#if 0
-    if (wht_check('v')) {
-      wht_require('v');
-      v = wht_read_int();
-    }
-#endif
     wht_require('[');
     n = wht_read_int();
     wht_require(']');
-    return wht_init_interleave(n,i,v);
+    return wht_init_interleave(n,i);
   } 
 
   return NULL;
