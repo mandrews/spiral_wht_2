a1b545572014ce575cfc26d83b1a387fc9e9a43c
Michael Andrews <mjands@gmail.com>
2007-08-28 18:13:17 +0000

    Fixed buffer length problem.
    git-svn-id: svn+ssh://tux.cs.drexel.edu/site/spiral/repos/spiral_wht_2/trunk@120 d1413626-1c2f-0410-9bee-9533c53eef12

--- wht/interleave.c
+++ wht/interleave.c
@@ -8,10 +8,7 @@ wht_init_interleave(size_t n, size_t i)
   const size_t bufsize = 20; /*apply_small%d_il%d\0*/
   char buf[bufsize]; 
 
-  snprintf(buf,bufsize,"apply_small%zd_v%zd_il%zd",n,2,i);
-#if 0
   snprintf(buf,bufsize,"apply_small%zd_il%zd",n,i);
-#endif
 
   W            = wht_init_codelet(n);
   W->apply     = wht_get_codelet(buf);
@@ -22,6 +19,26 @@ wht_init_interleave(size_t n, size_t i)
 
   return W;  
 }
+
+Wht *
+wht_init_interleave_vector(size_t n, size_t i)
+{
+  Wht *W;
+  const size_t bufsize = 24; /*apply_small%d_v%d_il%d\0*/
+  char buf[bufsize]; 
+
+  snprintf(buf,bufsize,"apply_small%zd_v%zd_il%zd",n,2,i);
+
+  W            = wht_init_codelet(n);
+  W->apply     = wht_get_codelet(buf);
+  W->nILNumber = i;
+
+  if (W->apply == NULL)
+    wht_error("Could not find codelet %s.", buf);
+
+  return W;  
+}
+
 
 Wht *
 wht_parse_interleave()
@@ -35,6 +52,18 @@ wht_parse_interleave()
     wht_require(']');
     return wht_init_interleave(n,i);
   } 
+
+  /*
+   * NOTE: This a hack, due the inability to represent this kind of codelet in
+   * the present grammar.
+   */
+  if (wht_is_codelet("smallvil")) {
+    i = wht_read_int();
+    wht_require('[');
+    n = wht_read_int();
+    wht_require(']');
+    return wht_init_interleave_vector(n,i);
+  }
 
   return NULL;
 }
