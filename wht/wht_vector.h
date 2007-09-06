b4e92007b623dbb9b5e6fab401bb030d57a640bf
Michael Andrews <mjands@gmail.com>
2007-09-06 18:04:54 +0000

    These macros should work with both gcc and icc
    git-svn-id: svn+ssh://tux.cs.drexel.edu/site/spiral/repos/spiral_wht_2/trunk@149 d1413626-1c2f-0410-9bee-9533c53eef12

--- wht/wht_vector.h
+++ wht/wht_vector.h
@@ -1,69 +1,49 @@
 #ifndef WHT_VECTOR_H
 #define WHT_VECTOR_H
 
-#ifdef __GNUC__
-
-typedef int v2si __attribute__ ((vector_size(8))); /* size in bytes */
-
 #ifdef WHT_DOUBLE 
-typedef double v2df __attribute__ ((vector_size(16))); /* size in bytes */
+#include <emmintrin.h>
 
-typedef v2df wht_vector2;
-
-/* 
- * From: gcc/x86_64-pc-linux-gnu/3.4.4/include/emmintrin.h
- */
+typedef __m128d wht_vector2;
 
 #define vadd2(R0,R1,R2) \
-  R0 = __builtin_ia32_addpd(R1,R2)
+  R0 = _mm_add_pd(R1,R2)
 
 #define vsub2(R0,R1,R2) \
-  R0 = __builtin_ia32_subpd(R1,R2)
+  R0 = _mm_sub_pd(R1,R2)
 
 #define vload2(R0,M0) \
-  R0 = __builtin_ia32_loadapd(&M0)
+  R0 = _mm_load_pd(&M0)
 
 #define vstore2(R0,M0) \
-  __builtin_ia32_storeapd(&M0,R0)
-
-#define MM_SHUFFLE2(R1,R0) \
- (((R1) << 1) | (R0))
+  _mm_store_pd(&M0,R0)
 
 #define vshuf2(R0,R1,R2,A,B) \
-  R0 = __builtin_ia32_shufpd(R1, R2, MM_SHUFFLE2(A,B))
+  R0 = _mm_shuffle_pd(R1, R2, _MM_SHUFFLE2(A,B))
 
 #endif/*WHT_DOUBLE*/
 
 #ifdef WHT_FLOAT 
-typedef float v4sf __attribute__ ((vector_size(16))); /* size in bytes */
+#include <xmmintrin.h>
 
-typedef v4sf wht_vector4;
-
-/* 
- * From: gcc/x86_64-pc-linux-gnu/3.4.4/include/emmintrin.h
- */
+typedef __m128 wht_vector4;
 
 #define vadd4(R0,R1,R2) \
-  R0 = __builtin_ia32_addps(R1,R2)
+  R0 = _mm_add_ps(R1,R2)
 
 #define vsub4(R0,R1,R2) \
-  R0 = __builtin_ia32_subps(R1,R2)
+  R0 = _mm_sub_ps(R1,R2)
 
 #define vload4(R0,M0) \
-  R0 = __builtin_ia32_loadaps(&M0)
+  R0 = _mm_load_ps(&M0)
 
 #define vstore4(R0,M0) \
-  __builtin_ia32_storeaps(&M0,R0)
-
-#define MM_SHUFFLE4(R3,R2,R1,R0) \
- (((R3) << 6) | ((R2) << 4) | ((R1) << 2) | (R0))
+  _mm_store_ps(&M0,R0)
 
 #define vshuf4(R0,R1,R2,A,B,C,D) \
-  R0 = __builtin_ia32_shufps(R1, R2, MM_SHUFFLE4(A,B,C,D))
+  R0 = _mm_shuffle_ps(R1, R2, MM_SHUFFLE(A,B,C,D))
 
 #endif/*WHT_FLOAT*/
 
-
-#endif/*__GNUC__*/
 
 #endif/*WHT_VECTOR_H*/
