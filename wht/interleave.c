e1a24073d71b3bc949b3c5b2b0ebbdcf6cd48a99
Michael Andrews <mjands@gmail.com>
2007-10-24 16:29:16 +0000

    Reverted back to previous revision and broke versioning of the file.
    git-svn-id: svn+ssh://tux.cs.drexel.edu/site/spiral/repos/spiral_wht_2/trunk@264 d1413626-1c2f-0410-9bee-9533c53eef12

---
+++ wht/interleave.c
@@ -0,0 +1,152 @@
+#include "wht.h"
+
+bool
+interleave_accept(Wht *W)
+{
+  if (W->parent == NULL) {
+    error_msg_set("codelet smallil(%d)[%d] must be used in conjunction with splitil", 
+      W->attr[interleave_by],
+      W->n);
+    return false;
+  }
+
+  if (strncmp("splitil", W->parent->name, strlen("splitil"))) {
+    error_msg_set("codelet smallil(%d)[%d] must be used in conjunction with splitil", 
+      W->attr[interleave_by],
+      W->n);
+
+    return false;
+  }
+
+  if (W->attr[interleave_by] > W->right) {
+    error_msg_set("interleave factor %d must be < %d in smallil(%d)[%d]", 
+      W->attr[interleave_by],
+      W->right,
+      W->attr[interleave_by],
+      W->n);
+    return false;
+  } 
+    
+  return true;
+}
+
+Wht *
+interleave_init(char *name, size_t n, int params[], size_t np)
+{
+  Wht *W;
+  size_t k;
+
+  k = params[0];
+
+  W            = small_init(name, n, params, np);
+  W->accept    = interleave_accept;
+
+  if (k > (1 << WHT_MAX_INTERLEAVE))
+    wht_error("not configured for codelets of size %zd interleaved by %zd", n, k);
+
+  W->attr[interleave_by] = k;
+
+  return W;  
+}
+
+bool
+split_interleave_accept(Wht *W)
+{
+  return split_accept(W);
+}
+
+void 
+split_interleave_apply(Wht *W, long S, size_t D, wht_value *x)
+{
+  int nn;
+  long N, R, S1, Ni, i, j, k;
+  size_t nk;
+  Wht *Wi;
+
+  nn = W->children->nn;
+
+  N  = W->N;
+  R  = N;
+  S1 = 1;
+
+
+  for (i = 0; i < nn; i++) {
+    Wi = W->children->Ws[i];
+    Ni = W->children->ns[i];
+    R /= Ni;
+
+    if (Wi->attr[interleave_by] != -1)
+      nk = Wi->attr[interleave_by];
+    else
+      nk = 1;
+
+    for (j = 0; j < R; j++)
+      for (k = 0; k < S1; k+=nk)
+        (Wi->apply)(Wi, S1*S, S, x+k*S+j*Ni*S1*S);
+
+
+    S1 *= Ni;
+  }
+}
+
+Wht *
+split_interleave_init(char *name, Wht *Ws[], size_t nn, int params[], size_t np) 
+{
+  Wht *W;
+
+  W            = split_init(name, Ws, nn, params, np);
+  W->apply     = split_interleave_apply;
+  W->accept    = split_interleave_accept;
+
+  return W;  
+}
+
+/** \todo This should go into external extensions header */
+Wht *
+interleave_convert(Wht *W, size_t k);
+
+Wht *
+small_interleave_convert(Wht *W, size_t k)
+{
+  size_t kp, k_min;
+  int *params;
+
+  if (W->right < (1 << WHT_MAX_INTERLEAVE))
+    k_min = W->right;
+  else
+    k_min = (1 << WHT_MAX_INTERLEAVE);
+ 
+  for (kp = k; kp > k_min; kp--);
+
+  if (kp < 2) 
+    return small_init("small", W->n, NULL, 0);
+
+  params = i_malloc(sizeof(*params)*1);
+  params[0] = kp;
+
+  return interleave_init("smallil", W->n, params, 1);
+}
+
+Wht *
+split_interleave_convert(Wht *W, size_t k)
+{
+  size_t nn, i;
+  Wht **Ws;
+
+  nn = W->children->nn;
+  Ws = i_malloc(sizeof(*Ws) *nn);
+
+  for (i = 0; i < nn; i++) 
+    Ws[i] = interleave_convert(W->children->Ws[i], k);
+
+  return split_interleave_init("splitil", Ws, nn, NULL, 0);
+}
+
+Wht *
+interleave_convert(Wht *W, size_t k)
+{
+  if (W->children)
+    return split_interleave_convert(W, k);
+  else
+    return small_interleave_convert(W, k);
+}
