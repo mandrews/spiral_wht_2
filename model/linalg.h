35814e1a791ce15e51b655e277f26a840acf21d2
Michael Andrews <mjands@gmail.com>
2007-11-12 20:14:06 +0000

    Added linear algebra helper routines.
    git-svn-id: svn+ssh://tux.cs.drexel.edu/site/spiral/repos/spiral_wht_2/trunk@277 d1413626-1c2f-0410-9bee-9533c53eef12

---
+++ model/linalg.h
@@ -0,0 +1,31 @@
+#ifndef LINALG_H
+#define LINALG_H
+
+struct matrix {
+  double *v;
+  size_t m;
+  size_t n;
+};
+
+struct qr_set {
+  struct matrix *q;
+  struct matrix *r;
+};
+
+struct matrix * matrix_init(size_t m, size_t n);
+
+void matrix_free(struct matrix *a);
+
+void matrix_print(struct matrix *a);
+
+void matrix_row_add(struct matrix *a, size_t k, double row[], size_t p);
+
+void matrix_col_add(struct matrix *a, size_t k, double col[], size_t p);
+
+struct matrix * matrix_transpose(struct matrix *a);
+
+struct qr_set* modified_gram_schmidt(struct matrix *a);
+
+struct matrix * matrix_least_squares_error(struct matrix *a, struct matrix *b);
+
+#endif/*LINALG_H*/
