71ff3a9ad6a4555c876d9d2ee8b62ea923e0f731
Michael Andrews <mjands@gmail.com>
2008-01-04 15:58:16 +0000

    Added more functionality to count see script.
    git-svn-id: svn+ssh://tux.cs.drexel.edu/site/spiral/repos/spiral_wht_2/trunk@369 d1413626-1c2f-0410-9bee-9533c53eef12

--- model/ic/count_sse.sh
+++ model/ic/count_sse.sh
@@ -2,52 +2,64 @@
 
 function usage() 
 {
-  echo "count_see [shuffle | unpack | scalar_add | vector_add | scalar_add | scalar_mov]"
+  echo "count_see wht_plan [shuffle | unpack | scalar_add | vector_add | scalar_add | scalar_mov]"
   exit 1
 }
 
 path=`dirname $0`
 
-v=2
-m=4
-i=8
-s=$1
+vsize=2
+unroll=4
+ifactor=8
+plan=$1
+mode=$2
 
-case $s in
+case ${mode} in
   'shuffle')
-    ops='[shuf]';
+    ops='shuf';
     ;;
   'unpack')
-    ops='[unpcklpd|unpckhpd]';
+    ops='unpcklpd|unpckhpd';
     ;;
   'scalar_add')
-    ops='[addsd|subsd]';
+    ops='addsd|subsd';
     ;;
   'vector_add')
-    ops='[addpd|subpd]';
+    ops='addpd|subpd';
     ;;
   'scalar_mov')
-    ops='[movsd|movlpd|movhpd]';
+    ops='movsd|movlpd|movhpd';
     ;;
   'vector_mov')
-    ops='[movpd|movapd]';
+    ops='movpd|movapd';
     ;;
   *)
     usage;
     ;;
 esac;
 
-for ((n=v;n<=m;n+=1)); do
-  x=`objdump -d ${path}/../../wht/codelets/s_${n}_v_${v}_a.o  | grep ${ops} | wc -l`
-  echo "smallv($v)[$n] : $x"
+tmp=`mktemp`
+
+for ((n=1;n<=unroll;n+=1)); do
+  x=`objdump -d ${path}/../../wht/codelets/s_${n}.o  | grep -E ${ops} | wc -l`
+  echo "small[$n] : $x" >> ${tmp}
 done
 
-for ((n=1;n<=m;n+=1)); do
-  for ((k=2;k<=i;k*=2)); do
+for ((n=vsize;n<=unroll;n+=1)); do
+  x=`objdump -d ${path}/../../wht/codelets/s_${n}_v_${vsize}_a.o  | grep -E ${ops} | wc -l`
+  echo "smallv($vsize)[$n] : $x" >> ${tmp}
+done
+
+for ((n=1;n<=unroll;n+=1)); do
+  for ((k=2;k<=ifactor;k*=2)); do
     # TODO fix the v2
-    x=`objdump -d ${path}/../../wht/codelets/s_${n}_il_${k}_v${v}.o  | grep ${ops} | wc -l`
-    echo "smallv($v,$k,0)[$n] : $x"
-    x=`objdump -d ${path}/../../wht/codelets/s_${n}_il_${k}_v${v}_a.o  | grep ${ops} | wc -l`
-    echo "smallv($v,$k,1)[$n] : $x"
+    x=`objdump -d ${path}/../../wht/codelets/s_${n}_il_${k}_v${vsize}.o  | grep -E ${ops} | wc -l`
+    echo "smallv($vsize,$k,0)[$n] : $x" >> ${tmp}
+    x=`objdump -d ${path}/../../wht/codelets/s_${n}_il_${k}_v${vsize}_a.o  | grep -E ${ops} | wc -l`
+    echo "smallv($vsize,$k,1)[$n] : $x" >> ${tmp}
   done
 done
+
+${path}/wht_count -w $plan | ${path}/wht_predict -f ${tmp}
+
+rm ${tmp}
