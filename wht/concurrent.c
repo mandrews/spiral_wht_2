6f1efb5b77b8ca18cad623df1c47805d5a0b136e
Michael Andrews <mjands@gmail.com>
2008-01-25 17:06:16 +0000

    Ported Kangs custom openMP code.
    git-svn-id: svn+ssh://tux.cs.drexel.edu/site/spiral/repos/spiral_wht_2/trunk@426 d1413626-1c2f-0410-9bee-9533c53eef12

--- wht/concurrent.c
+++ wht/concurrent.c
@@ -44,3 +44,92 @@ split_concurrent_apply(Wht *W, long S, size_t U, wht_value *x)
     S1 *= Ni;
   }
 }
+
+void 
+split_tmp_apply(Wht *W, long S, size_t U, wht_value *x) 
+{
+  long block, chunk;
+  long N, R, S1, Ni, j;
+  wht_value *xpt;
+  long total, id;
+
+  if (omp_in_parallel()) {
+    N  = W->N;
+    Ni = W->children->ns[1];
+    R = W->children->ns[0];
+
+    block = Ni * S;
+    for (j = 0, xpt = x; j < R; ++ j, xpt += block)
+      (W->children->Ws[1]->apply)(W->children->Ws[1], S, 1, xpt);
+
+    for (j = 0, xpt = x; j < Ni; ++ j, xpt += S)
+      (W->children->Ws[0]->apply)(W->children->Ws[0], block, 1, xpt);
+  } 
+
+  else {
+#pragma omp parallel private (xpt, id, j, chunk) 
+    { /* start of parallel region */
+      N  = W->N;
+      Ni = W->children->ns[1];
+      R = W->children->ns[0];
+      total = omp_get_num_threads();
+      block = Ni * S;  
+
+      /* no scheduling
+      id = omp_get_thread_num();
+      while (id < R) {
+        xpt = x + id * block;  
+        (W->children->Ws[1]->apply)(W->children->Ws[1], S, 1, xpt);
+        id += total;
+      }*/
+
+      chunk = R / total;
+      if (chunk * total == R) {
+        id = omp_get_thread_num() * chunk;
+        for (j = 0; j < chunk; ++ j, ++ id) {
+          xpt = x + id * block;
+          (W->children->Ws[1]->apply)(W->children->Ws[1], S, 1, xpt);
+        }
+      } else {
+        ++ chunk;
+        id = omp_get_thread_num() * chunk;
+        if (id <= R - chunk) {
+          for (j = 0; j < chunk; ++ j, ++ id) {
+            xpt = x + id * block;
+            (W->children->Ws[1]->apply)(W->children->Ws[1], S, 1, xpt);
+          }
+        } else if (id < R) {
+          for (; id < R; ++ id) {
+            xpt = x + id * block;
+            (W->children->Ws[1]->apply)(W->children->Ws[1], S, 1, xpt);
+          }
+        }
+      }
+
+#pragma omp barrier
+      chunk = Ni / total;
+      if (chunk * total == Ni) {
+        id = omp_get_thread_num() * chunk;
+        for (j = 0; j < chunk; ++ j, ++ id) {
+          xpt = x + id * S;
+          (W->children->Ws[0]->apply)(W->children->Ws[0], block, 1, xpt);
+        }
+      } else {
+        ++ chunk;
+        id = omp_get_thread_num() * chunk;
+        if (id <= Ni - chunk) {
+          for (j = 0; j < chunk; ++ j, ++ id) {
+            xpt = x + id * S;
+            (W->children->Ws[0]->apply)(W->children->Ws[0], block, 1, xpt);
+          }
+        } else if (id < Ni) {
+          for (; id < Ni; ++ id) {
+            xpt = x + id * S;
+            (W->children->Ws[0]->apply)(W->children->Ws[0], block, 1, xpt);
+          }
+        }
+      }
+    } /*end of parallel region*/
+  }
+}
+
