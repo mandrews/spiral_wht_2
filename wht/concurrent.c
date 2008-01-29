46a898cd25918e7980b308f5dbc4ac202920e226
Michael Andrews <mjands@gmail.com>
2008-01-29 22:51:02 +0000

    Experimental code for openMP stuff.
    git-svn-id: svn+ssh://tux.cs.drexel.edu/site/spiral/repos/spiral_wht_2/trunk@431 d1413626-1c2f-0410-9bee-9533c53eef12

--- wht/concurrent.c
+++ wht/concurrent.c
@@ -39,9 +39,14 @@ void split_concurrent_apply(Wht *W, long S, size_t U, wht_value *x);
 void
 split_concurrent_rule(Wht *W)
 {
+  size_t b;
+
   if (W->children == NULL)
     return error_msg_set(W, "codelet must be split to be interleaved");
 
+  b = (1 << W->rule->params[0]);
+
+  W->attr[omp_chunk] = b;
   W->apply = split_concurrent_apply;
 }
 
@@ -51,6 +56,9 @@ split_concurrent_apply(Wht *W, long S, size_t U, wht_value *x)
   int nn;
   long N, R, S1, Ni, i, j, k;
   Wht *Wi;
+  int b;
+  
+  b = W->attr[omp_chunk];
 
   nn = W->children->nn;
 
@@ -59,17 +67,118 @@ split_concurrent_apply(Wht *W, long S, size_t U, wht_value *x)
   S1 = 1;
 
   /* Iterate over children WHTs, stored anti lexigraphically  */
+  #pragma omp parallel for ordered schedule(guided, b) \
+    shared(i,Ni,Wi,R,S1) private(j,k) 
   for (i = 0; i < nn; i++) {
-    Ni = W->children->ns[i];
-    Wi = W->children->Ws[i];
-    R /= Ni;
+    #pragma omp ordered
+    {
+      Ni = W->children->ns[i];
+      Wi = W->children->Ws[i];
+      R /= Ni;
+    }
 
-    #pragma omp parallel for private(k) schedule(guided, OMP_CHUNK)
     for (j = 0; j < R; j++) 
       for (k = 0; k < S1; k++) 
         (Wi->apply)(Wi, S1*S, S, x+k*S+j*Ni*S1*S);
 
-    S1 *= Ni;
+    #pragma omp ordered 
+    {
+      S1 *= Ni;
+    }
   }
 }
 
+#if 0
+
+/* Kang's scheduled code from previous version of package.  Early testing seems
+ * to indicate that that openMP scheduling does a better job.
+ */
+
+void 
+split_kang_apply(Wht *W, long S, size_t U, wht_value *x) 
+{
+  long block, chunk;
+  long N, R, S1, Ni, j;
+  wht_value *xpt;
+  long total, id;
+
+  if (omp_in_parallel()) {
+    N  = W->N;
+    Ni = W->children->ns[1];
+    R = W->children->ns[0];
+
+    block = Ni * S;
+    for (j = 0, xpt = x; j < R; ++ j, xpt += block)
+      (W->children->Ws[1]->apply)(W->children->Ws[1], S, 1, xpt);
+
+    for (j = 0, xpt = x; j < Ni; ++ j, xpt += S)
+      (W->children->Ws[0]->apply)(W->children->Ws[0], block, 1, xpt);
+  } 
+
+  else {
+#pragma omp parallel private (xpt, id, j, chunk) 
+    { /* start of parallel region */
+      N  = W->N;
+      Ni = W->children->ns[1];
+      R = W->children->ns[0];
+      total = omp_get_num_threads();
+      block = Ni * S;  
+
+      /* no scheduling
+      id = omp_get_thread_num();
+      while (id < R) {
+        xpt = x + id * block;  
+        (W->children->Ws[1]->apply)(W->children->Ws[1], S, 1, xpt);
+        id += total;
+      }*/
+
+      chunk = R / total;
+      if (chunk * total == R) {
+        id = omp_get_thread_num() * chunk;
+        for (j = 0; j < chunk; ++ j, ++ id) {
+          xpt = x + id * block;
+          (W->children->Ws[1]->apply)(W->children->Ws[1], S, 1, xpt);
+        }
+      } else {
+        ++ chunk;
+        id = omp_get_thread_num() * chunk;
+        if (id <= R - chunk) {
+          for (j = 0; j < chunk; ++ j, ++ id) {
+            xpt = x + id * block;
+            (W->children->Ws[1]->apply)(W->children->Ws[1], S, 1, xpt);
+          }
+        } else if (id < R) {
+          for (; id < R; ++ id) {
+            xpt = x + id * block;
+            (W->children->Ws[1]->apply)(W->children->Ws[1], S, 1, xpt);
+          }
+        }
+      }
+
+#pragma omp barrier
+      chunk = Ni / total;
+      if (chunk * total == Ni) {
+        id = omp_get_thread_num() * chunk;
+        for (j = 0; j < chunk; ++ j, ++ id) {
+          xpt = x + id * S;
+          (W->children->Ws[0]->apply)(W->children->Ws[0], block, 1, xpt);
+        }
+      } else {
+        ++ chunk;
+        id = omp_get_thread_num() * chunk;
+        if (id <= Ni - chunk) {
+          for (j = 0; j < chunk; ++ j, ++ id) {
+            xpt = x + id * S;
+            (W->children->Ws[0]->apply)(W->children->Ws[0], block, 1, xpt);
+          }
+        } else if (id < Ni) {
+          for (; id < Ni; ++ id) {
+            xpt = x + id * S;
+            (W->children->Ws[0]->apply)(W->children->Ws[0], block, 1, xpt);
+          }
+        }
+      }
+    } /*end of parallel region*/
+  }
+}
+#endif
