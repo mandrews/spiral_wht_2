a922013340f305de5105c906abc23dd798590735
Michael Andrews <mjands@gmail.com>
2008-02-07 22:24:37 +0000

    Added new rule script interface to dp.
    git-svn-id: svn+ssh://tux.cs.drexel.edu/site/spiral/repos/spiral_wht_2/trunk@455 d1413626-1c2f-0410-9bee-9533c53eef12

--- extra/utils.rb
+++ extra/utils.rb
@@ -7,9 +7,6 @@
 
 require 'json'
 
-UTILS_PATH      = File.dirname(__FILE__) 
-BIN_PATH        = "#{UTILS_PATH}/../bin"
-
 INF = 1.0 / 0
 
 def load_runtime_env(measure)
@@ -56,86 +53,5 @@ def time(&blk)
   yield
   t1 = Time.now
   $stderr.puts t1 - t0
-end
-
-def wht_measure(plan, *args)
-  cmd = "#{BIN_PATH}/wht_measure -s -w '#{plan}' #{args}"
-  puts "Executing #{cmd}" if @debug
-  t = 0
-  IO.popen(cmd) do |fd|
-    t = fd.gets
-  end
-
-  if t.nil?
-    puts("Could not read: #{cmd}") if @debug
-    return INF
-  end
-
-  a = t.split(/\s+/)
-  return [ 
-    a[0].to_f,
-    a[1].to_f,
-    a[2].to_i
-  ]
-end
-
-def wht_classify(plan, *args)
-  cmd = "#{BIN_PATH}/wht_classify -w '#{plan}' #{args}"
-  puts "Executing #{cmd}" if @debug
-  t = 0
-  IO.popen(cmd) do |fd|
-    t = fd.readlines
-  end
-
-  h = {}
-  t.each do |l|
-    k,v = l.split(/\s*:\s*/)
-    h[k] = v.chomp
-  end
-
-  if h.empty?
-    puts("Could not read: #{cmd}") if @debug
-  end
-
-  return h
-end
-
-def wht_attach(plan, rules)
-  return plan if rules.empty?
-  dup = rules.dup
-
-  r0  = dup.shift
-  cmd = "#{BIN_PATH}/wht_attach -w '#{plan}' -r '#{r0}' "
-  dup.each do |ri|
-    cmd += "| #{BIN_PATH}/wht_attach -r '#{ri}' "
-  end
-
-  puts "Executing #{cmd}" if @debug
-  t = 0
-  IO.popen(cmd) do |fd|
-    t = fd.gets
-  end
-
-  unless t 
-    puts("Could not read: #{cmd}") if @debug
-    return plan
-  else
-    return t.chomp
-  end
-end
-
-def count_sse_sh(plan)
-  h = {}
-  ['shuffle', 'unpack', 'scalar_add', 'vector_add', 'scalar_mov', 'vector_mov'].each do |mode|
-    cmd = "bash #{BIN_PATH}/count_sse.sh '#{plan}' #{mode}"
-    puts "Executing #{cmd}"  if @debug
-    t = 0
-    IO.popen(cmd) do |fd|
-      t = fd.readlines
-    end
-
-    h[mode] = t.first.to_i
-  end
-  return h
 end
 
