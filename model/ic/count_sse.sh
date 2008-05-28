528450ea68050ae3dc4d94e8e2d2d45b95538bb6
Michael Andrews <mjands@gmail.com>
2008-05-28 19:20:28 +0000

    Generalized this script to count asm regexs and memory ops
    git-svn-id: svn+ssh://tux.cs.drexel.edu/site/spiral/repos/spiral_wht_2/trunk@513 d1413626-1c2f-0410-9bee-9533c53eef12

--- model/ic/count_sse.sh
+++ model/ic/count_sse.sh
@@ -2,7 +2,7 @@
 
 function usage() 
 {
-  echo "count_see -w wht_plan [shuffle | unpack | scalar_add | vector_add | scalar_mov | vector_mov]"
+  echo "count_see -w wht_plan -i instruction [-m]"
   exit 1
 }
 
@@ -11,67 +11,58 @@ vsize=`${path}/wht_predict -v | grep vector_size: | cut -d ' ' -f 2`
 unroll=`${path}/wht_predict -v | grep max_unroll: | cut -d ' ' -f 2`
 ifactor=`${path}/wht_predict -v | grep max_interleave: | cut -d ' ' -f 2`
 
-plan='';
+wht='';
+ins=''
+mem=0
 
-while getopts "w:" i; do
+while getopts "w:i:m" i; do
   case "$i" in
-    "w") plan=$OPTARG;
+    "w") wht=$OPTARG;
+    ;;
+    "i") ins=$OPTARG;
+    ;;
+    "m") mem=1;
     ;;
   esac
 done
 
-if [ "$plan" == "" ] ; then
-  read plan
-  mode=$1
-else
-  mode=$3
+if [ "$wht" == "" ] ; then
+  read wht
 fi
 
-case ${mode} in
-  'shuffle')
-    ops='shuf';
-    ;;
-  'unpack')
-    ops='unpcklpd|unpckhpd';
-    ;;
-  'scalar_add')
-    ops='addsd|subsd|addss|subss';
-    ;;
-  'vector_add')
-    ops='addpd|subpd|addps|subps';
-    ;;
-  'scalar_mov')
-    ops='movsd|movlpd|movhpd|movhps|movlpd|movss';
-    ;;
-  'vector_mov')
-    ops='movpd|movapd|movps|movaps';
-    ;;
-  *)
-    usage;
-    ;;
-esac;
+if [ "$mem" == 1 ]; then
+  cmd=""
+else
+  cmd="-v"
+fi
 
 mkdir -p "${path}/../share"
-tmp="${path}/../share/${mode}"
+md5=`echo "${ins} ${mem}" | md5sum | cut -d ' ' -f 1`
+tmp="${path}/../share/${md5}"
 
 if [ ! -e $tmp ]; then
   for ((n=1;n<=unroll;n+=1)); do
-    x=`objdump -d ${path}/../../wht/codelets/s_${n}.o  | grep -E ${ops} | wc -l`
+    f="${path}/../../wht/codelets/s_${n}.o"
+    x=`objdump -d ${f} | grep -E ${ins} | grep -E ${cmd} "\(|\)" | wc -l`
     echo "small[$n] : $x" >> ${tmp}
   done
 
   for ((n=1;n<=unroll;n+=1)); do
-    x=`objdump -d ${path}/../../wht/codelets/s_${n}_v_${vsize}_a.o  | grep -E ${ops} | wc -l`
+    f="${path}/../../wht/codelets/s_${n}_v_${vsize}_a.o"
+    x=`objdump -d ${f} | grep -E ${ins} | grep -E ${cmd} "\(|\)" | wc -l`
     echo "smallv($vsize)[$n] : $x" >> ${tmp}
   done
 
   for ((n=1;n<=unroll;n+=1)); do
     for ((k=2;k<=ifactor;k*=2)); do
-      x=`objdump -d ${path}/../../wht/codelets/s_${n}_il_${k}_v_${vsize}.o  | grep -E ${ops} | wc -l`
+      f="${path}/../../wht/codelets/s_${n}_il_${k}_v_${vsize}.o"
+      x=`objdump -d ${f} | grep -E ${ins} | grep -E ${cmd} "\(|\)" | wc -l`
       echo "smallv($vsize,$k,0)[$n] : $x" >> ${tmp}
-      x=`objdump -d ${path}/../../wht/codelets/s_${n}_il_${k}_v_${vsize}_a.o  | grep -E ${ops} | wc -l`
+      f="${path}/../../wht/codelets/s_${n}_il_${k}_v_${vsize}_a.o"
+      x=`objdump -d ${f} | grep -E ${ins} | grep -E ${cmd} "\(|\)" | wc -l`
       echo "smallv($vsize,$k,1)[$n] : $x" >> ${tmp}
-      x=`objdump -d ${path}/../../wht/codelets/s_${n}_il_${k}.o  | grep -E ${ops} | wc -l`
+      f="${path}/../../wht/codelets/s_${n}_il_${k}.o"
+      x=`objdump -d ${f} | grep -E ${ins} | grep -E ${cmd} "\(|\)" | wc -l`
       echo "smallil($k)[$n] : $x" >> ${tmp}
     done
   done
@@ -89,5 +80,5 @@ if [ ! -e $tmp ]; then
   mv ${tmp}~ ${tmp}
 fi
 
-${path}/wht_count -b ${tmp} -w $plan | ${path}/wht_predict -f ${tmp}
+${path}/wht_count -b ${tmp} -w $wht | ${path}/wht_predict -f ${tmp}
 
