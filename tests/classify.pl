a730737868db156f94a2f37341dac06697f13742
Michael Andrews <mjands@gmail.com>
2008-06-05 15:08:26 +0000

    Updated these tests to fit new definition of tree.
    git-svn-id: svn+ssh://tux.cs.drexel.edu/site/spiral/repos/spiral_wht_2/trunk@523 d1413626-1c2f-0410-9bee-9533c53eef12

--- tests/classify.pl
+++ tests/classify.pl
@@ -9,7 +9,7 @@ expect_classify
   smalld(1), 
 
   { 'left_tree'   => 0,
-    'right_node'  => 1 };
+    'right_node'  => 2 };
 
 expect_classify   
 
@@ -17,7 +17,7 @@ expect_classify
     smalld(1), smalld(1)), 
 
   { 'left_tree'   => 0,
-    'right_node'  => 1 };
+    'right_node'  => 4 };
 
 expect_classify   
 
@@ -26,8 +26,8 @@ expect_classify
       smalld(1),smalld(1)),
     smalld(1)), 
 
-  { 'left_tree'   => 2,
-    'right_node'  => 1 };
+  { 'left_tree'   => 16,
+    'right_node'  =>  8 };
 
 expect_classify   
 
@@ -36,16 +36,16 @@ expect_classify
       smalld(4),smalld(2)),
     smalld(1)), 
 
-  { 'left_tree'   => 6,
-    'right_node'  => 1 };
+  { 'left_tree'   => 256,
+    'right_node'  => 128 };
 
 expect_classify
   splitd(
     smalld(2), splitd(
       smalld(1), smalld(2))),
 
-   { 'left_tree'  => 0,
-     'right_node' => 2 };
+   { 'left_tree'  =>  0,
+     'right_node' => 32 };
 
 expect_classify   
 
@@ -55,8 +55,8 @@ expect_classify
     splitd(
       smalld(1),smalld(2))), 
       
-   { 'left_tree'  => 6,
-     'right_node' => 2 };
+   { 'left_tree'  => 1024,
+     'right_node' =>  512 };
 
 expect_classify   
 
@@ -69,6 +69,6 @@ expect_classify
     splitd(
       smalld(1),smalld(2))), 
       
-   { 'left_tree'  => 11,
-     'right_node' =>  2 };
+   { 'left_tree'  => 65536 ,
+     'right_node' => 16384 };
 
