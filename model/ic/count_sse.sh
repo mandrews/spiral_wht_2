9739f6d7999bbb5ca6da0291e213fcf0f49795ae
Michael Andrews <mjands@gmail.com>
2008-07-02 17:29:52 +0000

    Cleanedup count_sse script.
    git-svn-id: svn+ssh://tux.cs.drexel.edu/site/spiral/repos/spiral_wht_2/trunk@550 d1413626-1c2f-0410-9bee-9533c53eef12

--- model/ic/count_sse.sh
+++ model/ic/count_sse.sh
@@ -2,7 +2,7 @@
 
 function usage() 
 {
-  echo "count_see -w wht_plan -i instruction [-m]"
+  echo "count_see -w wht_plan -i instruction [-m] [-r]"
   exit 1
 }
 
@@ -13,15 +13,18 @@ ifactor=`${path}/wht_predict -v | grep max_interleave: | cut -d ' ' -f 2`
 
 wht='';
 ins=''
-mem=0
+mem=0 # Count memory ops
+reg=0 # Count register ops
 
-while getopts "w:i:m" i; do
+while getopts "w:i:mr" i; do
   case "$i" in
     "w") wht=$OPTARG;
     ;;
     "i") ins=$OPTARG;
     ;;
     "m") mem=1;
+    ;;
+    "r") reg=1;
     ;;
   esac
 done
@@ -34,43 +37,53 @@ if [ "$wht" == "" ] ; then
   read wht
 fi
 
-if [ "$mem" == 1 ]; then
-  cmd=""
-else
-  cmd="-v"
+if (( ("$mem" == 1) && ("$reg" == 1) )); then
+  echo "Cannot set both -m and -r"
+  usage
 fi
 
+function count() 
+{
+  file=$1
+  name=$2
+
+  f="${path}/../../wht/codelets/${file}.o"
+
+  # Skip if codelet doesn't exist
+  if [ ! -f $f ]; then
+    return;
+  fi
+
+  if [ "$mem" == 1 ]; then
+    x=`objdump -d ${f} | grep -E ${ins} | grep -E "\(|\)" | wc -l`;
+  elif [ "$reg" == 1 ]; then
+    x=`objdump -d ${f} | grep -E ${ins} | grep -E -v "\(|\)" | wc -l`;
+  else
+    x=`objdump -d ${f} | grep -E ${ins} | wc -l`;
+  fi
+
+  echo "$name : $x" >> ${tmp}
+}
+
 mkdir -p "${path}/../share"
-md5=`echo "${ins} ${mem}" | md5sum | cut -d ' ' -f 1`
+md5=`echo "${ins} ${mem} ${reg}" | md5sum | cut -d ' ' -f 1`
 tmp="${path}/../share/${md5}"
 
 if [ ! -e $tmp ]; then
   for ((n=1;n<=unroll;n+=1)); do
-    f="${path}/../../wht/codelets/s_${n}.o"
-    x=`objdump -d ${f} | grep -E ${ins} | grep -E ${cmd} "\(|\)" | wc -l`
-    echo "small[$n] : $x" >> ${tmp}
+    count "s_${n}" "small[${n}]"
   done
 
   for ((n=1;n<=unroll;n+=1)); do
-    f="${path}/../../wht/codelets/s_${n}_v_${vsize}_a.o"
-    x=`objdump -d ${f} | grep -E ${ins} | grep -E ${cmd} "\(|\)" | wc -l`
-    echo "smallv($vsize,1)[$n] : $x" >> ${tmp}
-    f="${path}/../../wht/codelets/s_${n}_v_${vsize}.o"
-    x=`objdump -d ${f} | grep -E ${ins} | grep -E ${cmd} "\(|\)" | wc -l`
-    echo "smallv($vsize,0)[$n] : $x" >> ${tmp}
+    count "s_${n}_v_${vsize}_a" "smallv($vsize,1)[$n]"
+    count "s_${n}_v_${vsize}" "smallv($vsize,0)[$n]"
   done
 
   for ((n=1;n<=unroll;n+=1)); do
     for ((k=2;k<=ifactor;k*=2)); do
-      f="${path}/../../wht/codelets/s_${n}_il_${k}_v_${vsize}.o"
-      x=`objdump -d ${f} | grep -E ${ins} | grep -E ${cmd} "\(|\)" | wc -l`
-      echo "smallv($vsize,$k,0)[$n] : $x" >> ${tmp}
-      f="${path}/../../wht/codelets/s_${n}_il_${k}_v_${vsize}_a.o"
-      x=`objdump -d ${f} | grep -E ${ins} | grep -E ${cmd} "\(|\)" | wc -l`
-      echo "smallv($vsize,$k,1)[$n] : $x" >> ${tmp}
-      f="${path}/../../wht/codelets/s_${n}_il_${k}.o"
-      x=`objdump -d ${f} | grep -E ${ins} | grep -E ${cmd} "\(|\)" | wc -l`
-      echo "smallil($k)[$n] : $x" >> ${tmp}
+      count "s_${n}_il_${k}_v_${vsize}" "smallv($vsize,$k,0)[$n]"
+      count "s_${n}_il_${k}_v_${vsize}_a" "smallv($vsize,$k,1)[$n]"
+      count "s_${n}_il_${k}" "smallil($k)[$n]"
     done
   done
   
