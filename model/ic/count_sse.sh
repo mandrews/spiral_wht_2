ff369c1366a6d5713e23c2d45f460c097fb08c8d
Michael Andrews <mjands@gmail.com>
2008-07-03 18:09:42 +0000

    Check in for count_sse which dissembles the library instead of object code files.  Significantly slower so I will be reverting back.
    git-svn-id: svn+ssh://tux.cs.drexel.edu/site/spiral/repos/spiral_wht_2/trunk@555 d1413626-1c2f-0410-9bee-9533c53eef12

--- model/ic/count_sse.sh
+++ model/ic/count_sse.sh
@@ -2,7 +2,10 @@
 
 function usage() 
 {
-  echo "count_see -w wht_plan -i instruction [-m] [-r]"
+  echo "count_see -w wht_plan -i instruction [-m] [-r] [-x]"
+  echo "  -m            Count memory to register instructions."
+  echo "  -r            Count register to register instructions."
+  echo "  -x            Clear the instruction count cache."
   exit 1
 }
 
@@ -15,8 +18,9 @@ wht='';
 ins=''
 mem=0 # Count memory ops
 reg=0 # Count register ops
+clr=0
 
-while getopts "w:i:mr" i; do
+while getopts "w:i:mrx" i; do
   case "$i" in
     "w") wht=$OPTARG;
     ;;
@@ -25,6 +29,8 @@ while getopts "w:i:mr" i; do
     "m") mem=1;
     ;;
     "r") reg=1;
+    ;;
+    "x") clr=1;
     ;;
   esac
 done
@@ -42,27 +48,50 @@ if (( ("$mem" == 1) && ("$reg" == 1) )); then
   usage
 fi
 
+# Clear the cache
+if [ "$clr" == 1 ]; then
+  rm ${path}/../share/*
+fi
+
+if [ -f "$path/../../wht/.libs/libwht.a" ]; then
+# Location of library in development environment
+  lib="$path/../../wht/.libs/libwht.a"
+elif [ -f "$path/../lib/libwht.a" ]; then
+# Location of library in installed environment
+  lib="$path/../lib/libwht.a"
+else
+  echo "Cannot find libwht.a"
+  exit 1
+fi
+
 function count() 
 {
-  file=$1
-  name=$2
+  fn=$1
+  id=$2
 
-  f="${path}/../../wht/codelets/${file}.o"
-
-  # Skip if codelet doesn't exist
-  if [ ! -f $f ]; then
-    return;
-  fi
+  # This hack grabs function calls that match $fn, e.g. apply_small1>: ... ASM
+  export FN=$fn
+  call=`objdump -d $lib | \
+  perl -e '
+    $s = 0; 
+    while (<>) { 
+      # Start grabbing lines after this one matches
+      if ($_ =~ /$ENV{'FN'}>/) { $s = 1; } 
+      # Stop when you reach a single newline
+      if ($_ =~ /^\n$/) { $s = 0; } 
+      if ($s == 1) { print $_; } 
+    }'`
+  unset FN
 
   if [ "$mem" == 1 ]; then
-    x=`objdump -d ${f} | grep -E ${ins} | grep -E "\(|\)" | wc -l`;
+    x=`echo "${call}" | grep -E ${ins} | grep -E "\(|\)" | wc -l`;
   elif [ "$reg" == 1 ]; then
-    x=`objdump -d ${f} | grep -E ${ins} | grep -E -v "\(|\)" | wc -l`;
+    x=`echo "${call}" | grep -E ${ins} | grep -E -v "\(|\)" | wc -l`;
   else
-    x=`objdump -d ${f} | grep -E ${ins} | wc -l`;
+    x=`echo "${call}" | grep -E ${ins} | wc -l`;
   fi
 
-  echo "$name : $x" >> ${tmp}
+  echo "$id : $x" >> ${tmp}
 }
 
 mkdir -p "${path}/../share"
@@ -70,20 +99,27 @@ md5=`echo "${ins} ${mem} ${reg}" | md5sum | cut -d ' ' -f 1`
 tmp="${path}/../share/${md5}"
 
 if [ ! -e $tmp ]; then
-  for ((n=1;n<=unroll;n+=1)); do
-    count "s_${n}" "small[${n}]"
-  done
+  echo "Generating cache for instruction '$ins'" 1>&2
 
   for ((n=1;n<=unroll;n+=1)); do
-    count "s_${n}_v_${vsize}_a" "smallv($vsize,1)[$n]"
-    count "s_${n}_v_${vsize}" "smallv($vsize,0)[$n]"
+    count "apply_small${n}" "small[${n}]"
+  done
+
+  v=$vsize
+  p=$(($v >> 1)) # Log 2 of v, for v,v codelets
+
+  count "apply_small${p}_v${v}" "smallv($v)[$p]"
+
+  for ((n=1;n<=unroll;n+=1)); do
+    count "apply_small${n}_v${v}" "smallv($v,0)[$n]"
+    count "apply_small${n}_v${v}_a" "smallv($v,1)[$n]"
   done
 
   for ((n=1;n<=unroll;n+=1)); do
     for ((k=2;k<=ifactor;k*=2)); do
-      count "s_${n}_il_${k}_v_${vsize}" "smallv($vsize,$k,0)[$n]"
-      count "s_${n}_il_${k}_v_${vsize}_a" "smallv($vsize,$k,1)[$n]"
-      count "s_${n}_il_${k}" "smallil($k)[$n]"
+      count "apply_small${n}_v_${v}_il_${k}" "smallv($v,$k,0)[$n]"
+      count "apply_small${n}_v_${v}_il_${k}_a" "smallv($v,$k,1)[$n]"
+      count "apply_small${n}_il${k}" "smallil($k)[$n]"
     done
   done
   
